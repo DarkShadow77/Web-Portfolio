9743e2b97d64df9a0fda6b46d90588bb11bd26f2
David Adeshina <dadeshina4@gmail.com>
2024-03-04 12:39:31 +0100

    portfolio

--- typescript/index.ts
+++ typescript/index.ts
@@ -4,7 +4,6 @@ let navTab = document.querySelector(`#firstpage nav #navi ul a`) as HTMLAnchorEl
 var hiddenNavbar = document.getElementById("hidden_one") as HTMLElement;
 let hoverNavBg = document.getElementById("hover") as HTMLSpanElement;
 let activeNavBg = document.getElementById("active") as HTMLSpanElement;
-console.log(document.querySelector(`#firstpage nav #navi ul`));
 
 let navBarHoverIndex: number = 1;
 
@@ -34,17 +33,22 @@ function onHover(index: number) {
     if (index == 1) {
         hoverNavBg.style.left = "5.5%"
     }
-    if (index == 2) {
+    else if (index == 2) {
         hoverNavBg.style.left = "23.5%"
     }
-    if (index == 3) {
+    else if (index == 3) {
         hoverNavBg.style.left = "42.5%"
     }
-    if (index == 4) {
+    else if (index == 4) {
         hoverNavBg.style.left = "61.5%"
     }
-    if (index == 5) {
+    else if (index == 5) {
         hoverNavBg.style.left = "80.5%"
+    }
+    let hoverTab = document.querySelector(`#firstpage nav #navi ul :nth-child(${index}) :hover`) as HTMLAnchorElement
+    console.log(`${index} ${navBarHoverIndex}`)
+    if(navBarHoverIndex == index){
+        hoverTab.style.color = "red"
     }
 }
 
@@ -52,44 +56,48 @@ function onHoverOut(mouseHoverNumber?: number) {
     if (navBarHoverIndex == 1) {
         hoverNavBg.style.left = "5.5%"
     }
-    if (navBarHoverIndex == 2) {
+    else if (navBarHoverIndex == 2) {
         hoverNavBg.style.left = "25%"
     }
-    if (navBarHoverIndex == 3) {
+    else if (navBarHoverIndex == 3) {
         hoverNavBg.style.left = "44%"
     }
-    if (navBarHoverIndex == 4) {
+    else if (navBarHoverIndex == 4) {
         hoverNavBg.style.left = "63%"
     }
-    if (navBarHoverIndex == 5) {
+    else if (navBarHoverIndex == 5) {
         hoverNavBg.style.left = "82%"
     }
 }
 
 function onNavBarClick(index: number) {
+    for (let x = 1; x <= 5; x++) {
+        let ss = document.querySelector(`#firstpage nav #navi ul :nth-child(${x})`) as HTMLAnchorElement
+         ss.classList.remove("active")
+     }
     let selectedTab = document.querySelector(`#firstpage nav #navi ul :nth-child(${index})`) as HTMLAnchorElement
     navBarHoverIndex = index;
     if (index == 1) {
         activeNavBg.style.left = "4%"
-        
+
         selectedTab.classList.add("active")
     }
-    if (index == 2) {
+    else if (index == 2) {
         activeNavBg.style.left = "23.5%"
 
         selectedTab.classList.add("active")
     }
-    if (index == 3) {
+    else if (index == 3) {
         activeNavBg.style.left = "42.5%"
 
         selectedTab.classList.add("active")
     }
-    if (index == 4) {
+    else if (index == 4) {
         activeNavBg.style.left = "61.5%"
 
         selectedTab.classList.add("active")
     }
-    if (index == 5) {
+    else if (index == 5) {
         activeNavBg.style.left = "80.5%"
 
         selectedTab.classList.add("active")
