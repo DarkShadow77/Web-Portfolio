6ee8301b07f94e0acda35975c31cf370d8b17123
DarkShadow77 <dadeshina4@gmail.com>
2024-05-03 14:23:36 +0100

    Almost Done !

--- typescript/build/index.js
+++ typescript/build/index.js
@@ -7,23 +7,74 @@ let hoverNavBg = document.getElementById("hover");
 let activeNavBg = document.getElementById("active");
 let previousWork = document.getElementById("backButton");
 let nextWork = document.getElementById("nextButton");
-var skillContainer = document.getElementById("skill_container");
+let skillContainer = document.getElementById("skill_container");
 let navBarHoverIndex = 1;
 let skillArray = [
-    { skill: "Flutter", percent: 85, offset: 105 },
-    { skill: "Dart", percent: 75, offset: 105 },
-    { skill: "HTML", percent: 70, offset: 210 },
-    { skill: "Css", percent: 85, offset: 30 },
-    { skill: "Javascript", percent: 65, offset: 30 },
-    { skill: "Typescript", percent: 60, offset: 30 },
-    { skill: "React", percent: 40, offset: 30 },
+    { skill: "Flutter", percent: 85, offset: "105" },
+    { skill: "Dart", percent: 75, offset: "105" },
+    { skill: "HTML", percent: 70, offset: "210" },
+    { skill: "Css", percent: 70, offset: "30" },
+    { skill: "Javascript", percent: 65, offset: "30" },
+    { skill: "Typescript", percent: 60, offset: "30" },
+    { skill: "React", percent: 50, offset: "60" },
+    { skill: "Figma", percent: 40, offset: "60" },
+    { skill: "GitHub", percent: 60, offset: "60" },
 ];
 let workArray = [
     { name: "Simon Game", description: "An inquisitive Computer Science Engineering student, skilled in leadership, seeking to leverage solid development skills with focus on collaboration, communication and passion.", skills: [{ name: "HTML5" }, { name: "CSS3" }, { name: "Java Script" },], githubLink: "" },
-    { name: "Simon Game", description: "An inquisitive Computering to leverage solid development skills with focus on collaboration, communication and passion.", skills: [{ name: "HTML5" }, { name: "CSS3" }, { name: "Java Script" }, { name: "Flutter" },], githubLink: "" },
+    { name: "Todo Game", description: "An inquisitive Computering to leverage solid development skills with focus on collaboration, communication and passion.", skills: [{ name: "HTML5" }, { name: "CSS3" }, { name: "Java Script" }, { name: "Flutter" },], githubLink: "" },
+    { name: "Todo Game", description: "An inquisitive Computering to leverage solid development skills with focus on collaboration, communication and passion.", skills: [{ name: "HTML5" }, { name: "CSS3" }, { name: "Java Script" }, { name: "Flutter" },], githubLink: "" },
 ];
 let workIndex = 0;
 window.addEventListener("load", () => {
+    const sections = document.querySelectorAll("section");
+    const navLi = document.querySelectorAll("#firstpage nav #navi ul a");
+    window.addEventListener("scroll", () => {
+        let current = "";
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (scrollY >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute("id");
+            }
+        });
+        if (current == "firstpage") {
+            activeNavBg.style.left = "3%";
+            navBarHoverIndex = 1;
+            onHover(1);
+            onHoverOut();
+        }
+        else if (current == "secondpage") {
+            activeNavBg.style.left = "22.5%";
+            onHover(2);
+            navBarHoverIndex = 2;
+            onHoverOut();
+        }
+        else if (current == "thirdpage") {
+            activeNavBg.style.left = "42.5%";
+            onHover(3);
+            navBarHoverIndex = 3;
+            onHoverOut();
+        }
+        else if (current == "fourthpage") {
+            activeNavBg.style.left = "61.5%";
+            onHover(4);
+            navBarHoverIndex = 4;
+            onHoverOut();
+        }
+        else if (current == "fifthpage") {
+            activeNavBg.style.left = "80.5%";
+            onHover(5);
+            navBarHoverIndex = 5;
+            onHoverOut();
+        }
+        navLi.forEach((li) => {
+            li.classList.remove("active");
+            if (li.classList.contains(current)) {
+                li.classList.add("active");
+            }
+        });
+    });
     previousWork.style.display = "none";
     for (let x = 0; x < skillArray.length; x++) {
         skillContainer.innerHTML += `
@@ -40,41 +91,14 @@ window.addEventListener("load", () => {
                     <stop offset="100%" stop-color="#928A97" />
                   </linearGradient>
                 </defs>
-                <circle cx="50" cy="50" r="45" stroke-linecap="round" />
+                <circle cx="50" cy="50" r="45" stroke-linecap="round" class="skillCircle" />
               </svg>
               <span class="skillText">${skillArray[x].skill}</span>
             </div>
             `;
-        let loading = document.querySelectorAll(`circle`);
-        const keyFrames = document.createElement("style");
-        keyFrames.innerHTML = `
-                @keyframes anim {
-                    100% {
-                        stroke-dashoffset: ${skillArray[2].offset};
-                    }
-                }
-        
-                circle {
-                    animation: anim 2s linear forwards;
-                }
-            `;
-        loading[x].appendChild(keyFrames);
+        let loading = document.getElementsByClassName(`skillCircle`);
+        loading[x].style.setProperty('--m', skillArray[x].offset);
     }
-    // let loadings = document.querySelectorAll(`circle`) as NodeListOf<SVGCircleElement>
-    // loading.forEach((circle, index) => {
-    //     const keyFrames = document.createElement("style");
-    //     keyFrames.innerHTML = `
-    //     @keyframes anim {
-    //         100% {
-    //             stroke-dashoffset: ${skillArray[1].offset};
-    //         }
-    //     }
-    //     circle {
-    //         animation: anim 2s linear forwards;
-    //     }
-    // `;
-    //     circle.appendChild(keyFrames)
-    // });
     workFunc(workIndex);
 });
 window.addEventListener("scroll", () => {
@@ -95,30 +119,13 @@ window.addEventListener("scroll", () => {
         navContainer.style.boxShadow = "";
         navContainer.style.backdropFilter = "";
     }
-    // const sections = document.querySelectorAll("section");
-    // const navLi = document.querySelectorAll("#firstpage nav #navi ul a");
-    // let current: string = "";
-    // sections.forEach((section) => {
-    //     const sectionTop = section.offsetTop;
-    //     const sectionHeight = section.clientHeight;
-    //     if (scrollY >= sectionTop - sectionHeight / 3) {
-    //         current = section.getAttribute("id")!;
-    //     }
-    // });
-    // navLi.forEach((li, index) => {
-    // let selectedTab = document.querySelector(`#firstpage nav #navi ul :nth-child(${index+1})`) as HTMLAnchorElement
-    //     li.classList.remove("Navactive");
-    //     if (li.classList.contains(current)) {
-    //         li.classList.add("Navactive");
-    //     }
-    // });
 });
 function onHover(index) {
     if (index == 1) {
-        hoverNavBg.style.left = "4%";
+        hoverNavBg.style.left = "3%";
     }
     else if (index == 2) {
-        hoverNavBg.style.left = "23.5%";
+        hoverNavBg.style.left = "22.5%";
     }
     else if (index == 3) {
         hoverNavBg.style.left = "42.5%";
@@ -129,18 +136,13 @@ function onHover(index) {
     else if (index == 5) {
         hoverNavBg.style.left = "80.5%";
     }
-    let hoverTab = document.querySelector(`#firstpage nav #navi ul :nth-child(${index})`);
-    if (navBarHoverIndex == index) {
-        console.log("Chee");
-        hoverTab.style.color = "red";
-    }
 }
 function onHoverOut(mouseHoverNumber) {
-    if (navBarHoverIndex == 1) {
-        hoverNavBg.style.left = "5.5%";
+    if (navBarHoverIndex == 1 || navBarHoverIndex == 0) {
+        hoverNavBg.style.left = "4.5%";
     }
     else if (navBarHoverIndex == 2) {
-        hoverNavBg.style.left = "25%";
+        hoverNavBg.style.left = "24%";
     }
     else if (navBarHoverIndex == 3) {
         hoverNavBg.style.left = "44%";
@@ -153,46 +155,30 @@ function onHoverOut(mouseHoverNumber) {
     }
 }
 function onNavBarClick(index) {
-    for (let x = 1; x <= 5; x++) {
-        let ss = document.querySelector(`#firstpage nav #navi ul :nth-child(${x})`);
-        ss.style.color = "beige";
-        ss.classList.remove("Navactive");
-    }
-    let selectedTab = document.querySelector(`#firstpage nav #navi ul :nth-child(${index})`);
-    navBarHoverIndex = index;
     if (index == 1) {
         let element = document.getElementById("firstpage");
         element.scrollIntoView({ behavior: "smooth" });
-        activeNavBg.style.left = "4%";
-        selectedTab.style.color = "#F44336";
     }
     else if (index == 2) {
         let element = document.getElementById("secondpage");
         element.scrollIntoView({ behavior: "smooth" });
-        activeNavBg.style.left = "23.5%";
-        selectedTab.style.color = "#F44336";
     }
     else if (index == 3) {
         let element = document.getElementById("thirdpage");
         element.scrollIntoView({ behavior: "smooth" });
-        activeNavBg.style.left = "42.5%";
-        selectedTab.style.color = "#F44336";
     }
     else if (index == 4) {
         let element = document.getElementById("fourthpage");
         element.scrollIntoView({ behavior: "smooth" });
-        activeNavBg.style.left = "61.5%";
-        selectedTab.style.color = "#F44336";
     }
     else if (index == 5) {
         let element = document.getElementById("fifthpage");
         element.scrollIntoView({ behavior: "smooth" });
-        activeNavBg.style.left = "80.5%";
-        selectedTab.style.color = "#F44336";
     }
 }
 nextWork.addEventListener("click", () => {
-    workIndex++;
+    workIndex += 1;
+    console.log(workIndex);
     if (workIndex == ((workArray.length) - 1)) {
         nextWork.style.display = "none";
     }
@@ -231,11 +217,19 @@ function workFunc(index) {
     let workName = document.getElementById("work-name");
     let workDescription = document.getElementById("work-description");
     let workSkillContainer = document.getElementById("work-skills_container");
+    let indicatorContainer = document.getElementById("indicator_container");
+    let workCount = document.getElementById("work_index");
+    workCount.textContent = `${index < 9 && "0"}${index + 1}`;
     workName.textContent = workArray[index].name;
     workDescription.textContent = workArray[index].description;
     workSkillContainer.innerHTML = "";
+    indicatorContainer.innerHTML = "";
     for (let x = 0; x < workArray[index].skills.length; x++) {
         let skill = workArray[index].skills[x].name;
         workSkillContainer.innerHTML += `<div class="work-skills">${skill} </div>`;
+    }
+    for (let x = 0; x < workArray.length; x++) {
+        indicatorContainer.innerHTML +=
+            `<div class="indicator" id="${index == x && "active"}"></div>`;
     }
 }
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,7 +1,7 @@
 interface Skill {
     skill: string;
     percent: number;
-    offset: number;
+    offset: string;
 }
 
 interface Works {
@@ -19,29 +19,85 @@ let hoverNavBg = document.getElementById("hover") as HTMLSpanElement;
 let activeNavBg = document.getElementById("active") as HTMLSpanElement;
 let previousWork = document.getElementById("backButton") as HTMLButtonElement;
 let nextWork = document.getElementById("nextButton") as HTMLButtonElement;
-
-var skillContainer = document.getElementById("skill_container") as HTMLElement;
+let skillContainer = document.getElementById("skill_container") as HTMLElement;
 
 let navBarHoverIndex: number = 1;
 
 let skillArray: Skill[] = [
-    { skill: "Flutter", percent: 85, offset: 105 },
-    { skill: "Dart", percent: 75, offset: 105 },
-    { skill: "HTML", percent: 70, offset: 210 },
-    { skill: "Css", percent: 85, offset: 30 },
-    { skill: "Javascript", percent: 65, offset: 30 },
-    { skill: "Typescript", percent: 60, offset: 30 },
-    { skill: "React", percent: 40, offset: 30 },
+    { skill: "Flutter", percent: 85, offset: "105" },
+    { skill: "Dart", percent: 75, offset: "105" },
+    { skill: "HTML", percent: 70, offset: "210" },
+    { skill: "Css", percent: 70, offset: "30" },
+    { skill: "Javascript", percent: 65, offset: "30" },
+    { skill: "Typescript", percent: 60, offset: "30" },
+    { skill: "React", percent: 50, offset: "60" },
+    { skill: "Figma", percent: 40, offset: "60" },
+    { skill: "GitHub", percent: 60, offset: "60" },
 ]
 
 let workArray: Works[] = [
     { name: "Simon Game", description: "An inquisitive Computer Science Engineering student, skilled in leadership, seeking to leverage solid development skills with focus on collaboration, communication and passion.", skills: [{ name: "HTML5" }, { name: "CSS3" }, { name: "Java Script" },], githubLink: "" },
-    { name: "Simon Game", description: "An inquisitive Computering to leverage solid development skills with focus on collaboration, communication and passion.", skills: [{ name: "HTML5" }, { name: "CSS3" }, { name: "Java Script" }, { name: "Flutter" },], githubLink: "" },
+    { name: "Todo Game", description: "An inquisitive Computering to leverage solid development skills with focus on collaboration, communication and passion.", skills: [{ name: "HTML5" }, { name: "CSS3" }, { name: "Java Script" }, { name: "Flutter" },], githubLink: "" },
+    { name: "Todo Game", description: "An inquisitive Computering to leverage solid development skills with focus on collaboration, communication and passion.", skills: [{ name: "HTML5" }, { name: "CSS3" }, { name: "Java Script" }, { name: "Flutter" },], githubLink: "" },
 ]
 
 let workIndex: number = 0;
 
 window.addEventListener("load", () => {
+
+    const sections = document.querySelectorAll("section");
+    const navLi = document.querySelectorAll("#firstpage nav #navi ul a");
+
+    window.addEventListener("scroll", () => {
+        let current: string = "";
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (scrollY >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute("id")!;
+            }
+        });
+
+        if (current == "firstpage") {
+            activeNavBg.style.left = "3%"
+            navBarHoverIndex = 1;
+            onHover(1)
+            onHoverOut()
+        }
+        else if (current == "secondpage") {
+            activeNavBg.style.left = "22.5%"
+            onHover(2)
+            navBarHoverIndex = 2;
+            onHoverOut()
+        }
+        else if (current == "thirdpage") {
+            activeNavBg.style.left = "42.5%"
+            onHover(3)
+            navBarHoverIndex = 3;
+            onHoverOut()
+        }
+        else if (current == "fourthpage") {
+            activeNavBg.style.left = "61.5%"
+            onHover(4)
+            navBarHoverIndex = 4;
+            onHoverOut()
+        }
+        else if (current == "fifthpage") {
+            activeNavBg.style.left = "80.5%"
+            onHover(5)
+            navBarHoverIndex = 5;
+            onHoverOut()
+        }
+
+        navLi.forEach((li) => {
+            li.classList.remove("active");
+            if (li.classList.contains(current)) {
+                li.classList.add("active");
+            }
+
+        });
+    });
+
     previousWork.style.display = "none";
     for (let x = 0; x < skillArray.length; x++) {
         skillContainer.innerHTML += `
@@ -58,49 +114,17 @@ window.addEventListener("load", () => {
                     <stop offset="100%" stop-color="#928A97" />
                   </linearGradient>
                 </defs>
-                <circle cx="50" cy="50" r="45" stroke-linecap="round" />
+                <circle cx="50" cy="50" r="45" stroke-linecap="round" class="skillCircle" />
               </svg>
               <span class="skillText">${skillArray[x].skill}</span>
             </div>
             `;
 
-        let loading = document.querySelectorAll(`circle`) as NodeListOf<SVGCircleElement>;
+        let loading = document.getElementsByClassName(`skillCircle`) as HTMLCollectionOf<SVGCircleElement>;
 
-
-        const keyFrames = document.createElement("style");
-        keyFrames.innerHTML = `
-                @keyframes anim {
-                    100% {
-                        stroke-dashoffset: ${skillArray[2].offset};
-                    }
-                }
-        
-                circle {
-                    animation: anim 2s linear forwards;
-                }
-            `;
-
-        loading[x].appendChild(keyFrames);
+        loading[x].style.setProperty('--m', skillArray[x].offset);
 
     }
-    // let loadings = document.querySelectorAll(`circle`) as NodeListOf<SVGCircleElement>
-    // loading.forEach((circle, index) => {
-
-    //     const keyFrames = document.createElement("style");
-    //     keyFrames.innerHTML = `
-    //     @keyframes anim {
-    //         100% {
-    //             stroke-dashoffset: ${skillArray[1].offset};
-    //         }
-    //     }
-
-    //     circle {
-    //         animation: anim 2s linear forwards;
-    //     }
-    // `;
-    //     circle.appendChild(keyFrames)
-    // });
-
     workFunc(workIndex);
 });
 
@@ -121,36 +145,14 @@ window.addEventListener("scroll", () => {
         navContainer.style.boxShadow = "";
         navContainer.style.backdropFilter = "";
     }
-
-    // const sections = document.querySelectorAll("section");
-    // const navLi = document.querySelectorAll("#firstpage nav #navi ul a");
-    // let current: string = "";
-    // sections.forEach((section) => {
-    //     const sectionTop = section.offsetTop;
-    //     const sectionHeight = section.clientHeight;
-    //     if (scrollY >= sectionTop - sectionHeight / 3) {
-    //         current = section.getAttribute("id")!;
-    //     }
-    // });
-
-    // navLi.forEach((li, index) => {
-
-    // let selectedTab = document.querySelector(`#firstpage nav #navi ul :nth-child(${index+1})`) as HTMLAnchorElement
-
-    //     li.classList.remove("Navactive");
-    //     if (li.classList.contains(current)) {
-    //         li.classList.add("Navactive");
-    //     }
-
-    // });
 });
 
 function onHover(index: number) {
     if (index == 1) {
-        hoverNavBg.style.left = "4%"
+        hoverNavBg.style.left = "3%"
     }
     else if (index == 2) {
-        hoverNavBg.style.left = "23.5%"
+        hoverNavBg.style.left = "22.5%"
     }
     else if (index == 3) {
         hoverNavBg.style.left = "42.5%"
@@ -162,20 +164,14 @@ function onHover(index: number) {
         hoverNavBg.style.left = "80.5%"
     }
 
-    let hoverTab = document.querySelector(`#firstpage nav #navi ul :nth-child(${index})`) as HTMLAnchorElement
-
-    if (navBarHoverIndex == index) {
-        console.log("Chee")
-        hoverTab.style.color = "red"
-    }
 }
 
 function onHoverOut(mouseHoverNumber?: number) {
-    if (navBarHoverIndex == 1) {
-        hoverNavBg.style.left = "5.5%"
+    if (navBarHoverIndex == 1 || navBarHoverIndex == 0) {
+        hoverNavBg.style.left = "4.5%"
     }
     else if (navBarHoverIndex == 2) {
-        hoverNavBg.style.left = "25%"
+        hoverNavBg.style.left = "24%"
     }
     else if (navBarHoverIndex == 3) {
         hoverNavBg.style.left = "44%"
@@ -189,59 +185,39 @@ function onHoverOut(mouseHoverNumber?: number) {
 }
 
 function onNavBarClick(index: number) {
-    for (let x = 1; x <= 5; x++) {
-        let ss = document.querySelector(`#firstpage nav #navi ul :nth-child(${x})`) as HTMLAnchorElement
-        ss.style.color = "beige"
-        ss.classList.remove("Navactive")
-    }
-
-    let selectedTab = document.querySelector(`#firstpage nav #navi ul :nth-child(${index})`) as HTMLAnchorElement
-    navBarHoverIndex = index;
 
     if (index == 1) {
         let element = document.getElementById("firstpage") as HTMLElement;
         element.scrollIntoView({ behavior: "smooth" })
 
-        activeNavBg.style.left = "4%"
 
-        selectedTab.style.color = "#F44336"
     }
     else if (index == 2) {
         let element = document.getElementById("secondpage") as HTMLElement;
         element.scrollIntoView({ behavior: "smooth" })
 
-        activeNavBg.style.left = "23.5%"
-
-        selectedTab.style.color = "#F44336"
     }
     else if (index == 3) {
         let element = document.getElementById("thirdpage") as HTMLElement;
         element.scrollIntoView({ behavior: "smooth" })
 
-        activeNavBg.style.left = "42.5%"
-
-        selectedTab.style.color = "#F44336"
     }
     else if (index == 4) {
         let element = document.getElementById("fourthpage") as HTMLElement;
         element.scrollIntoView({ behavior: "smooth" })
 
-        activeNavBg.style.left = "61.5%"
-
-        selectedTab.style.color = "#F44336"
     }
     else if (index == 5) {
 
         let element = document.getElementById("fifthpage") as HTMLElement;
         element.scrollIntoView({ behavior: "smooth" })
-        activeNavBg.style.left = "80.5%"
 
-        selectedTab.style.color = "#F44336"
     }
 }
 
 nextWork.addEventListener("click", () => {
-    workIndex++
+    workIndex += 1
+    console.log(workIndex)
     if (workIndex == ((workArray.length) - 1)) {
         nextWork.style.display = "none";
     } else {
@@ -279,14 +255,24 @@ function workFunc(index: number) {
     let workName = document.getElementById("work-name") as HTMLHeadingElement
     let workDescription = document.getElementById("work-description") as HTMLParagraphElement
     let workSkillContainer = document.getElementById("work-skills_container") as HTMLParagraphElement
+    let indicatorContainer = document.getElementById("indicator_container") as HTMLElement;
+    let workCount = document.getElementById("work_index") as HTMLElement;
 
+
+    workCount.textContent = `${index < 9 && "0"}${index + 1}`
     workName.textContent = workArray[index].name;
     workDescription.textContent = workArray[index].description;
     workSkillContainer.innerHTML = "";
+    indicatorContainer.innerHTML = "";
 
     for (let x = 0; x < workArray[index].skills.length; x++) {
         let skill = workArray[index].skills[x].name;
         workSkillContainer.innerHTML += `<div class="work-skills">${skill} </div>`
-
     }
+
+    for (let x = 0; x < workArray.length; x++) {
+        indicatorContainer.innerHTML +=
+            `<div class="indicator" id="${index == x && "active"}"></div>`;
+    }
+
 }
